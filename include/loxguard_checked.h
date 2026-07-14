932f0130f919a5c534d9dd402f9412f5eed11413
Peter <Vanderhell@gmail.com>
2026-07-14 10:36:36 +0200

    fix: make loxguard arena alignment strict c99 compatible

--- include/loxguard_checked.h
+++ include/loxguard_checked.h
@@ -3,11 +3,19 @@
 
 #include "loxguard.h"
 
+#if defined(__clang__) || defined(__GNUC__)
+#define LOXGUARD_ALIGNOF(type) __alignof__(type)
+#elif defined(_MSC_VER)
+#define LOXGUARD_ALIGNOF(type) __alignof(type)
+#else
+#define LOXGUARD_ALIGNOF(type) offsetof(struct { char _loxguard_align; type _loxguard_member; }, _loxguard_member)
+#endif
+
 #define LOX_READ_U8(span_ptr, idx, out_ptr) lox_span_read_u8((span_ptr), (idx), (out_ptr))
 #define LOX_WRITE_U8(span_ptr, idx, val) lox_span_write_u8((span_ptr), (idx), (val))
 #define LOX_MEMCPY(dst_ptr, dst_off, src_ptr, src_off, len) \
     lox_span_memcpy((dst_ptr), (dst_off), (src_ptr), (src_off), (len))
 #define LOX_ARENA_ALLOC(arena_ptr, type, count) \
-    ((type *)loxguard_arena_alloc_array((arena_ptr), sizeof(type), (size_t)(count), offsetof(struct { char _loxguard_align; type _loxguard_member; }, _loxguard_member)))
+    ((type *)loxguard_arena_alloc_array((arena_ptr), sizeof(type), (size_t)(count), LOXGUARD_ALIGNOF(type)))
 
 #endif
